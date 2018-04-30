adcc72e3ea9122230dc394c88e675ef9b99df443
Christian Strebel <christian.strebel@axonivy.com>
2018-04-30 10:19:03 +0200

    Return nice version string e.g. Leading Edge for LE

--- src/app/action/VersionIndexAction.php
+++ src/app/action/VersionIndexAction.php
@@ -17,14 +17,29 @@ class VersionIndexAction
     public function __invoke(Request $request, $response, $args)
     {
         $version = $args['version'];
+        $versionStr = self::versionString($version);
         $p2DataPath = $this->container->get('settings')['p2DataPath'];
         $rootFolder = P2FileUtil::getRootFolder($request, $response, $p2DataPath, $version);
         $latestVersion = P2FileUtil::getLatestVersion($rootFolder);
         
         return $this->container->get('view')->render($response, 'version-index.html', [
             'version' => $version,
+            'versionStr' => $versionStr,
             'currentUri' => $this->container->get('request')->getUri(),
             'latestVersion' => $latestVersion
         ]);
+    }
+    
+    private function versionString($version) : string
+    {
+        switch ($version) {
+            case 'LE':
+                return 'Leading Edge';
+            case 'sprint':
+                return 'Sprint Release';
+            case 'nightly':
+                return 'Nightly Build';
+        }
+        return $version;
     }
 }
